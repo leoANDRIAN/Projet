7984e9c7502d3b336cc5be0a11c34d9e66b600e3
leoANDRIAN <leo.andrian@hotmail.com>
2021-06-16 14:19:13 +0200

    Initial

---
+++ animation.cpp
@@ -0,0 +1,12 @@
+#include "animation.h"
+
+Animation::Animation(double ph, double th, Vector accel, Vector speed, Point p)
+{
+    // Constructor
+    // Initialization
+    phi = ph;
+    theta = th;
+    acc = accel;
+    spd = speed;
+    pos = p;
+}
